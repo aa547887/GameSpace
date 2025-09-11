122d530b1340c3344e2066db72271aced1eca241
Shang Hsiang <aa0953693201@gmail.com>
2025-09-11 15:48:54 +0800

    2.0基板 合併會員

--- GameSpace/GameSpace/Areas/MemberManagement/Controllers/HomeController.cs
+++ GameSpace/GameSpace/Areas/MemberManagement/Controllers/HomeController.cs
@@ -5,6 +5,14 @@ namespace GameSpace.Areas.MemberManagement.Controllers
 	[Area("MemberManagement")]
 	public class HomeController : Controller
 	{
+		//public IActionResult User()
+		//{
+		//	return View();
+		//}
+		public IActionResult Manager()
+		{
+			return View();
+		}
 		public IActionResult Index()
 		{
 			return View();
---
+++ GameSpace/GameSpace/Areas/MemberManagement/Models/ManagerRolePermissionVM.cs
@@ -0,0 +1,33 @@
+﻿using GameSpace.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using GameSpace.Areas.MemberManagement.Models;
+
+namespace GameSpace.Areas.MemberManagement.Models
+{ 
+
+public partial class ManagerRolePermissionVM
+{
+    [Display(Name = "管理員角色編號")]
+	public int ManagerRoleId { get; set; }
+    [Display(Name = "管理員角色名稱")]
+	public string RoleName { get; set; } = string.Empty;
+		[Display(Name = "平台系統管理")] 
+	public bool? AdministratorPrivilegesManagement { get; set; }
+    [Display(Name = "使用者系統管理")]
+	public bool? UserStatusManagement { get; set; }
+    [Display(Name = "商品系統管理")]
+	public bool? ShoppingPermissionManagement { get; set; }
+    [Display(Name = "論壇系統管理")]
+
+	public bool? MessagePermissionManagement { get; set; }
+    [Display(Name = "寵物系統管理")]
+	public bool? PetRightsManagement { get; set; }
+    [Display(Name = "客服系統管理")]
+	public bool? CustomerService { get; set; }
+
+		public List<string> ManagerNames { get; set; } = new List<string>();
+		public ManagerRoleEntry ManagerRole { get; set; }
+		
+	}
+}
