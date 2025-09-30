5fd013a8ec8714f0b0c361bedba45b40f5e23328
鐘群能 <n20295459@gmail.com>
2025-10-01 06:53:09 +0800

    清理MiniGame區域：移除重複文件和更新相關配置

--- GameSpace/GameSpace/Data/ApplicationDbContext.cs
+++ GameSpace/GameSpace/Data/ApplicationDbContext.cs
@@ -1,6 +1,5 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using GameSpace.Areas.MiniGame.Models;
 
 namespace GameSpace.Data
 {
@@ -9,115 +8,6 @@ namespace GameSpace.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-        }
-
-        // MiniGame Area Entities
-        public DbSet<User> Users { get; set; }
-        public DbSet<UserIntroduce> UserIntroduces { get; set; }
-        public DbSet<UserRights> UserRights { get; set; }
-        public DbSet<UserWallet> UserWallets { get; set; }
-        public DbSet<WalletHistory> WalletHistories { get; set; }
-        public DbSet<UserSignInStats> UserSignInStats { get; set; }
-        public DbSet<Pet> Pets { get; set; }
-        public DbSet<MiniGame> MiniGames { get; set; }
-        public DbSet<Coupon> Coupons { get; set; }
-        public DbSet<EVoucher> EVouchers { get; set; }
-        public DbSet<EVoucherToken> EVoucherTokens { get; set; }
-        public DbSet<EVoucherRedeemLog> EVoucherRedeemLogs { get; set; }
-        public DbSet<SignInRuleSettings> SignInRuleSettings { get; set; }
-        public DbSet<PetSystemRuleSettings> PetSystemRuleSettings { get; set; }
-        public DbSet<MiniGameRuleSettings> MiniGameRuleSettings { get; set; }
-        public DbSet<PetAppearanceChangeLog> PetAppearanceChangeLogs { get; set; }
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            // User relationships
-            modelBuilder.Entity<User>()
-                .HasOne(u => u.UserIntroduce)
-                .WithOne(ui => ui.User)
-                .HasForeignKey<UserIntroduce>(ui => ui.User_ID);
-
-            modelBuilder.Entity<User>()
-                .HasOne(u => u.UserRights)
-                .WithOne(ur => ur.User)
-                .HasForeignKey<UserRights>(ur => ur.User_ID);
-
-            modelBuilder.Entity<User>()
-                .HasOne(u => u.UserWallet)
-                .WithOne(uw => uw.User)
-                .HasForeignKey<UserWallet>(uw => uw.User_ID);
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.UserSignInStats)
-                .WithOne(usis => usis.User)
-                .HasForeignKey(usis => usis.User_ID);
-
-            modelBuilder.Entity<User>()
-                .HasOne(u => u.Pet)
-                .WithOne(p => p.User)
-                .HasForeignKey<Pet>(p => p.User_ID);
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.MiniGames)
-                .WithOne(mg => mg.User)
-                .HasForeignKey(mg => mg.User_ID);
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.WalletHistories)
-                .WithOne(wh => wh.User)
-                .HasForeignKey(wh => wh.User_ID);
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.Coupons)
-                .WithOne(c => c.User)
-                .HasForeignKey(c => c.User_ID);
-
-            modelBuilder.Entity<User>()
-                .HasMany(u => u.EVouchers)
-                .WithOne(ev => ev.User)
-                .HasForeignKey(ev => ev.User_ID);
-
-            // Pet relationships
-            modelBuilder.Entity<Pet>()
-                .HasMany(p => p.PetAppearanceChangeLogs)
-                .WithOne(pacl => pacl.Pet)
-                .HasForeignKey(pacl => pacl.PetID);
-
-            // EVoucher relationships
-            modelBuilder.Entity<EVoucher>()
-                .HasMany(ev => ev.EVoucherTokens)
-                .WithOne(evt => evt.EVoucher)
-                .HasForeignKey(evt => evt.EVoucherID);
-
-            modelBuilder.Entity<EVoucher>()
-                .HasMany(ev => ev.EVoucherRedeemLogs)
-                .WithOne(evrl => evrl.EVoucher)
-                .HasForeignKey(evrl => evrl.EVoucherID);
-
-            modelBuilder.Entity<EVoucherToken>()
-                .HasMany(evt => evt.EVoucherRedeemLogs)
-                .WithOne(evrl => evrl.EVoucherToken)
-                .HasForeignKey(evrl => evrl.TokenID);
-
-            // Configure table names to match database schema
-            modelBuilder.Entity<User>().ToTable("Users");
-            modelBuilder.Entity<UserIntroduce>().ToTable("UserIntroduces");
-            modelBuilder.Entity<UserRights>().ToTable("UserRights");
-            modelBuilder.Entity<UserWallet>().ToTable("UserWallets");
-            modelBuilder.Entity<WalletHistory>().ToTable("WalletHistories");
-            modelBuilder.Entity<UserSignInStats>().ToTable("UserSignInStats");
-            modelBuilder.Entity<Pet>().ToTable("Pets");
-            modelBuilder.Entity<MiniGame>().ToTable("MiniGames");
-            modelBuilder.Entity<Coupon>().ToTable("Coupons");
-            modelBuilder.Entity<EVoucher>().ToTable("EVouchers");
-            modelBuilder.Entity<EVoucherToken>().ToTable("EVoucherTokens");
-            modelBuilder.Entity<EVoucherRedeemLog>().ToTable("EVoucherRedeemLogs");
-            modelBuilder.Entity<SignInRuleSettings>().ToTable("SignInRuleSettings");
-            modelBuilder.Entity<PetSystemRuleSettings>().ToTable("PetSystemRuleSettings");
-            modelBuilder.Entity<MiniGameRuleSettings>().ToTable("MiniGameRuleSettings");
-            modelBuilder.Entity<PetAppearanceChangeLog>().ToTable("PetAppearanceChangeLogs");
         }
     }
 }
