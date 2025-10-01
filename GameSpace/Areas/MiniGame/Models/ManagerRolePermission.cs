49ba60d7ca1cba6d5234e0b192f6baf1366ed6e4
鐘群能 <n20295459@gmail.com>
2025-10-01 12:45:41 +0800

    Add database connection documentation and update MiniGame models

--- GameSpace/Areas/MiniGame/Models/ManagerRolePermission.cs
+++ GameSpace/Areas/MiniGame/Models/ManagerRolePermission.cs
@@ -3,30 +3,36 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameSpace.Areas.MiniGame.Models
 {
-    /// <summary>
-    /// 管理者角色權限表
-    /// </summary>
     [Table("ManagerRolePermission")]
     public class ManagerRolePermission
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ManagerRole_Id")]
         public int ManagerRole_Id { get; set; }
 
+        [Column("role_name")]
         [Required]
         [StringLength(50)]
         public string role_name { get; set; } = string.Empty;
 
+        [Column("AdministratorPrivilegesManagement")]
         public bool AdministratorPrivilegesManagement { get; set; } = false;
 
+        [Column("UserStatusManagement")]
         public bool UserStatusManagement { get; set; } = false;
 
+        [Column("ShoppingPermissionManagement")]
         public bool ShoppingPermissionManagement { get; set; } = false;
 
+        [Column("MessagePermissionManagement")]
         public bool MessagePermissionManagement { get; set; } = false;
 
+        [Column("Pet_Rights_Management")]
         public bool Pet_Rights_Management { get; set; } = false;
 
+        [Column("customer_service")]
         public bool customer_service { get; set; } = false;
+
+        public virtual ICollection<ManagerRole> ManagerRoles { get; set; } = new List<ManagerRole>();
     }
 }
