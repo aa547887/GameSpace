693c02e7431634a5617916caf844d6100243affc
鐘群能 <n20295459@gmail.com>
2025-10-01 17:33:46 +0800

    feat(minigame-admin): [P1-1.3] 完成寵物等級經驗值設定、升級規則設定、背景成本設定、顏色成本設定功能

--- GameSpace/GameSpace/Areas/MiniGame/Models/ViewModels/PermissionViewModels.cs
+++ GameSpace/GameSpace/Areas/MiniGame/Models/ViewModels/PermissionViewModels.cs
@@ -291,14 +291,4 @@ namespace GameSpace.Areas.MiniGame.Models.ViewModels
     /// <summary>
     /// 分頁結果 ViewModel
     /// </summary>
-    public class PagedResult<T>
-    {
-        public List<T> Items { get; set; } = new();
-        public int TotalCount { get; set; }
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
-        public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
-        public bool HasPreviousPage => PageNumber > 1;
-        public bool HasNextPage => PageNumber < TotalPages;
-    }
 }
