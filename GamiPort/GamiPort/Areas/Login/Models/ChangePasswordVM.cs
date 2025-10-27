60e9ef1c67568e758e83bb5107f187040e945a2a
aa669119 <aa547887>
2025-10-27 19:22:24 +0800

    修bug & 新增照片顯示

---
+++ GamiPort/GamiPort/Areas/Login/Models/ChangePasswordVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamiPort.Areas.Login.Models
+{
+	public class ChangePasswordVM
+	{
+		[Required(ErrorMessage = "請輸入舊密碼")]
+		[DataType(DataType.Password)]
+		[Display(Name = "舊密碼")]
+		public string OldPassword { get; set; } = null!;
+
+		[Required(ErrorMessage = "請輸入新密碼")]
+		[DataType(DataType.Password)]
+		[StringLength(32, MinimumLength = 8, ErrorMessage = "密碼長度需介於 8~32 字元")]
+		[Display(Name = "新密碼")]
+		public string NewPassword { get; set; } = null!;
+
+		[Required(ErrorMessage = "請再次確認新密碼")]
+		[DataType(DataType.Password)]
+		[Compare(nameof(NewPassword), ErrorMessage = "兩次輸入的新密碼不一致")]
+		[Display(Name = "確認新密碼")]
+		public string ConfirmPassword { get; set; } = null!;
+	}
+}
---
+++ GamiPort/GamiPort/Services/ImagePathService.cs
@@ -0,0 +1,8 @@
+﻿namespace GamiPort.Services
+{
+	public static class ImagePathService
+	{
+		// 預設頭像的相對路徑
+		public const string DefaultUserAvatar = "/images/預設人物.png";
+	}
+}
