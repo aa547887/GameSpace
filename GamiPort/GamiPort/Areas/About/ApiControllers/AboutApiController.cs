50db71da82a0e8dd1c28941d16055977a5184e8e
Shang Hsiang <aa0953693201@gmail.com>
2025-11-06 02:16:28 +0800

    調版面

---
+++ GamiPort/GamiPort/Areas/About/ApiControllers/AboutApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamiPort.Areas.About.ApiControllers
+{
+	public class AboutApiController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
